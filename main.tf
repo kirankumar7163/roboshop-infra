d6136a20024da0ea5b1b31d2c7b24dafcbf225d4
kiran1616 <kirankumar7163@gmail.com>
2023-03-16 18:36:43 +0530

    updated script

--- main.tf
+++ main.tf
@@ -1,5 +1,4 @@
-resource "null_resource" "test" {
-  provisioner "local-exec" {
-    command = "echo ${var.env}"
-  }
+module "network" {
+  source  = "github.com/kirankumar7163/tf-module-vpc"
+  version = "main"
 }
