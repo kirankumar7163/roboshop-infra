a65512fd641ba656098c1fee3733b59f54034f97
kiran1616 <kirankumar7163@gmail.com>
2023-03-30 22:53:51 +0530

    updated script

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-module "network" {
+module "vpc" {
   source  = "github.com/kirankumar7163/tf-module-vpc"
   env = var.env
   default_vpc_id = var.default_vpc_id
@@ -14,8 +14,8 @@ module "subnets" {
   env = var.env
   default_vpc_id = var.default_vpc_id
 
-  vpc_id = module.network.vpc_id
+  vpc_id = module.vpc.vpc_id
 
-  for_each = var.vpc
-  cidr_block           = each.value.cidr_block
+  for_each      = var.vpc
+  cidr_block    = each.value.cidr_block
 }
