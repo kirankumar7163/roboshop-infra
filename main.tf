02604bd9e8493aafcba7d153dc27f13c1ff0265b
kiran1616 <kirankumar7163@gmail.com>
2023-03-31 00:02:31 +0530

    updated script

--- main.tf
+++ main.tf
@@ -9,15 +9,15 @@ module "vpc" {
 
 
 
-#module "subnets" {
-#  source                = "github.com/kirankumar7163/tf-module-subnets"
-#  env                   = var.env
-#  default_vpc_id        = var.default_vpc_id
+module "subnets" {
+  source                = "github.com/kirankumar7163/tf-module-subnets"
+  env                   = var.env
+  default_vpc_id        = var.default_vpc_id
 
-#  vpc_id                = lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_id", null)
+  vpc_id                = lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_id", null)
 
-#  for_each      = var.subnets
-#  cidr_block    = each.value.cidr_block
-#  availability_zone = each.value.availability_zone
-#  name = each.value.name
-#}
+  for_each      = var.subnets
+  cidr_block    = each.value.cidr_block
+  availability_zone = each.value.availability_zone
+  name = each.value.name
+}
