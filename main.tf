5d9fd5eab45a579c0c2720622eb420d4d7a6d958
kiran1616 <kirankumar7163@gmail.com>
2023-04-11 18:56:56 +0530

    updated script

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ module "docdb" {
   env    = var.env
 
   for_each   = var.docdb
-  subnet_ids = lookup(lookup(lookup(lookup(module.vpc, each.value.vpc_name, null), "private_subnet_ids", null), each.value.subnets_name, null), "subnet_ids", null)
+  subnet_ids = lookup(lookup(lookup(lookup(module.vpc, each.value.vpc_name, null), "private_subnets_ids", null), each.value.subnets_name, null), "subnet_ids", null)
 }
 
 output "vpc" {
